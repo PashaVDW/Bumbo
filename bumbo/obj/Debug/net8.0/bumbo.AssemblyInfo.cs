ad554bc583e4286da85a91a84f7bcbd9e2720486
Tjjvdo <tjj.vandenouweland@student.avans.nl>
2024-10-08 13:24:14 +0200

    add Modal
    kan modal nog niet goed testen wegens afwezigheid van de volledige opzet en CDN van Tailwind

--- bumbo/obj/Debug/net8.0/bumbo.AssemblyInfo.cs
+++ bumbo/obj/Debug/net8.0/bumbo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bumbo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7d790fc7e0aba07ca5f106d3d62702005b18e24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5d38d954f7ef5fb0a97aef23717df653497ea39")]
 [assembly: System.Reflection.AssemblyProductAttribute("bumbo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bumbo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
