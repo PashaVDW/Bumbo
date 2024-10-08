e7d790fc7e0aba07ca5f106d3d62702005b18e24
Tjjvdo <tjj.vandenouweland@student.avans.nl>
2024-10-08 11:36:45 +0200

    teruggeplaatst voor testing

--- bumbo/obj/Debug/net8.0/bumbo.AssemblyInfo.cs
+++ bumbo/obj/Debug/net8.0/bumbo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bumbo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0148619770dc13e91a1caea0b1a0fc62c73212d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1367ddd2c1f0a4571f3d364aa64a947fe4142f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("bumbo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bumbo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
