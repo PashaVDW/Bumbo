6c4879580555c2eb009a702c0c90913957299a17
ant-george <agross@live.nl>
2024-10-10 16:53:11 +0200

    Custom button, custom style toegepast

--- bumbo/Components/CustomButton.cs
+++ bumbo/Components/CustomButton.cs
@@ -8,22 +8,28 @@ namespace bumbo.Components
         public string ButtonText { get; set; } = "klik hier";
         public string ButtonType { get; set; }
         public string OnClick { get; set; }
+        public string CustomStyle { get; set; }
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             output.TagName = "button";
 
+            string customStyle = !string.IsNullOrEmpty(CustomStyle) ? CustomStyle : string.Empty;
+
             string buttonClass = ButtonType?.ToLower() switch
             {
-                "primary" => "primary_bg_color hover:bg-yellow-400 text-white font-semibold py-2 px-6 rounded-xl",
-                "secondary" => "bg-gray-600 hover:bg-gray-500 text-white font-semibold py-2 px-6 rounded-xl",
-                _ => "primary_bg_color hover:bg-yellow-400 text-white font-semibold py-2 px-6 rounded-xl"
+                "primary" => $"primary_bg_color hover:bg-yellow-400 text-white font-semibold py-2 px-6 rounded-xl {customStyle}",
+                "secondary" => $"bg-gray-600 hover:bg-gray-500 text-white font-semibold py-2 px-6 rounded-xl {customStyle}",
+                _ => $"primary_bg_color hover:bg-yellow-400 text-white font-semibold py-2 px-6 rounded-xl {customStyle}"
             };
+
             output.Attributes.SetAttribute("class", buttonClass);
+
             if (!string.IsNullOrEmpty(OnClick))
             {
                 output.Attributes.SetAttribute("onclick", OnClick);
             }
+
             output.Content.SetContent(ButtonText);
         }
     }
