3d1f8c71cde3e5baeca613ae0c4b24926f993328
ant-george <agross@live.nl>
2024-10-10 15:41:56 +0200

    Login view klaar
    View van login is volledig klaar.
    Daarnaast werkt de login ook nu in de happy flow variant. Nu moet er natuurlijk ook een melding koment wanneer het fout gaat.

---
+++ bumbo/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using bumbo.Models;
+using bumbo.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+public class AccountController : Controller
+{
+    private readonly SignInManager<Employee> _signInManager;
+
+    public AccountController(SignInManager<Employee> signInManager)
+    {
+        _signInManager = signInManager;
+    }
+
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var result = await _signInManager.PasswordSignInAsync(
+                model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError("", "Invalid login attempt.");
+        }
+
+        return View(model);
+    }
+
+    public async Task<IActionResult> Logout()
+    {
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Index", "Home");
+    }
+}
