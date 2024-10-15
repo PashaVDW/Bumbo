c5b5888fe3c8152f9ab28a8b0ae24338ff5e8721
Tjjvdo <tjj.vandenouweland@student.avans.nl>
2024-10-15 10:27:47 +0200

    branch update

---
+++ bumbo/Models/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace bumbo.Models
+{
+    public class Country
+    {
+        [Key]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        // Een Country kan meerdere branches hebben
+        public ICollection<Branch> Branches { get; set; }
+    }
+}
---
+++ bumbo/Views/Shared/Components/NavBarViewComponent.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using bumbo.Models;
+using bumbo.ViewModels;
+
+namespace bumbo.ViewComponents
+{
+    public class NavBarViewComponent : ViewComponent
+    {
+        private readonly UserManager<Employee> _userManager;
+
+        public NavBarViewComponent(UserManager<Employee> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var model = new NavBarViewModel();
+
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = await _userManager.GetUserAsync(HttpContext.User);
+
+                if (user != null)
+                {
+                    model.FirstName = user.FirstName;
+                    model.Email = user.Email;
+                    model.IsSystemManager = user.IsSystemManager;
+                    model.IsAuthenticated = true;
+                    model.IsBranchManager = user.ManagerOfBranchId != null;
+                }
+            }
+            else
+            {
+                model.IsAuthenticated = false;
+            }
+
+            return View(model);
+        }
+    }
+}
+
+
