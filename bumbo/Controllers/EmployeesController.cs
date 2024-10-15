18bbcc02e16ed68d7b3d7ca900498dd084038405
ant-george <agross@live.nl>
2024-10-15 12:13:20 +0200

    Controller aanpassing Post

--- bumbo/Controllers/EmployeesController.cs
+++ bumbo/Controllers/EmployeesController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
+using bumbo.Models;
+using bumbo.ViewModels;
+using System.Threading.Tasks;
 
 namespace bumbo.Controllers
 {
@@ -15,12 +18,71 @@ namespace bumbo.Controllers
         public async Task<IActionResult> Index()
         {
             var user = await _userManager.GetUserAsync(User);
-            
-            if (user == null || user.ManagerOfBranchId == null)
+
+            if (user == null || (user.ManagerOfBranchId == null && !user.IsSystemManager))
             {
                 return RedirectToAction("AccessDenied", "Home");
             }
             return View();
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user == null || (user.ManagerOfBranchId == null && !user.IsSystemManager))
+            {
+                return RedirectToAction("AccessDenied", "Home");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CreateEmployeeViewModel model)
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user == null || (user.ManagerOfBranchId == null && !user.IsSystemManager))
+            {
+                return RedirectToAction("AccessDenied", "Home");
+            }
+
+            if (ModelState.IsValid)
+            {
+                var employee = new Employee
+                {
+                    FirstName = model.FirstName,
+                    MiddleName = model.MiddleName,
+                    LastName = model.LastName,
+                    BirthDate = model.BirthDate,
+                    PostalCode = model.PostalCode,
+                    HouseNumber = model.HouseNumber,
+                    StartDate = model.StartDate,
+                    FunctionName = model.FunctionName,
+                    Email = model.Email,
+                    UserName = model.Email,
+                    IsSystemManager = model.IsSystemManager,
+                    ManagerOfBranchId = model.ManagerOfBranchId
+                };
+
+                var result = await _userManager.CreateAsync(employee, "DefaultPassword123!");
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            return View(model);
+        }
+
     }
 }
--- bumbo/obj/Debug/net8.0/bumbo.AssemblyInfo.cs
+++ bumbo/obj/Debug/net8.0/bumbo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bumbo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b99ae4bba6111d518dcf8deae302f04920a5d09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df72fb73fef876ba7a53408c3448611b996caa7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("bumbo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bumbo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
