cb7b61e7ad2ff8ab62215864b215ab7c828f3a24
JuIes2004 <167075889+JuIes2004@users.noreply.github.com>
2024-10-15 11:11:29 +0200

    Read front-end voortgang

--- bumbo/Controllers/NormsController.cs
+++ bumbo/Controllers/NormsController.cs
@@ -11,6 +11,8 @@ using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualBasic;
 using System.Globalization;
+using bumbo.Components;
+using static bumbo.Controllers.NormeringController;
 
 namespace bumbo.Controllers;
 
@@ -28,17 +30,81 @@ public class NormsController : Controller
         _context = context;
     }
 
-    public async Task<IActionResult> Index()
+    //public async Task<IActionResult> Index()
+    //{
+    //    var norms = _context.Norms.ToList();
+
+    //    var user = await _userManager.GetUserAsync(User);
+
+    //    if (user == null || user.ManagerOfBranchId == null)
+    //    {
+    //        return RedirectToAction("AccessDenied", "Home");
+    //    }
+    //    return View(norms);
+    //}
+
+    public IActionResult Index(string searchTerm, int page = 1)
     {
-        var norms = _context.Norms.ToList();
+        var query = _context.Norms
+            .Where(coli => coli.activity == "Coli uitladen")
+            .Join(_context.Norms.Where(shelve => shelve.activity == "Vakkenvullen"),
+                  coli => new { coli.branchId, coli.year, coli.week },
+                  shelve => new { shelve.branchId, shelve.year, shelve.week },
+                  (coli, shelve) => new { coli, shelve })
+            .Join(_context.Norms.Where(cashier => cashier.activity == "Kassa"),
+                  result => new { result.coli.branchId, result.coli.year, result.coli.week },
+                  cashier => new { cashier.branchId, cashier.year, cashier.week },
+                  (result, cashier) => new { result.coli, result.shelve, cashier })
+            .Join(_context.Norms.Where(fresh => fresh.activity == "Vers"),
+                  result => new { result.coli.branchId, result.coli.year, result.coli.week },
+                  fresh => new { fresh.branchId, fresh.year, fresh.week },
+                  (result, fresh) => new { result.coli, result.shelve, result.cashier, fresh })
+            .Join(_context.Norms.Where(front => front.activity == "Spiegelen"),
+                  result => new { result.coli.branchId, result.coli.year, result.coli.week },
+                  front => new { front.branchId, front.year, front.week },
+                  (result, front) => new
+                  {
+                      Week = result.coli.week,
+                      ColiNorm = result.coli.normInSeconds,
+                      ShelveNorm = result.shelve.normInSeconds,
+                      CashierNorm = result.cashier.normInSeconds,
+                      FreshNorm = result.fresh.normInSeconds,
+                      FrontNorm = front.normInSeconds
+                  });
 
-        var user = await _userManager.GetUserAsync(User);
+        var norms = query.ToList();
+        List<ReadNormViewModel> list = new List<ReadNormViewModel>();
 
-        if (user == null || user.ManagerOfBranchId == null)
+        foreach (var norm in norms)
         {
-            return RedirectToAction("AccessDenied", "Home");
+            var item = new ReadNormViewModel();
+
+            item.Week = norm.Week;
+            item.UnloadColis = norm.ColiNorm/60;
+            item.FillShelves = norm.ShelveNorm/60;
+            item.Cashier = norm.CashierNorm;
+            item.Fresh = norm.FreshNorm;
+            item.Fronting = norm.FrontNorm;
+
+            list.Add(item);
         }
-        return View(norms);
+
+        var headers = new List<string> { "Week", "Coli uitladen", "Vakken vullen", "Kassa", "Vers", "Spiegelen" };
+        var tableBuilder = new TableHtmlBuilder<ReadNormViewModel>();
+        var htmlTable = tableBuilder.GenerateTable("Normeringen", headers, list, "../#add", "../#edit", item =>
+        {
+            return $@"
+                    <td class='py-2 px-4'>{item.Week}</td>
+                    <td class='py-2 px-4'>{item.UnloadColis} minuten</td>
+                    <td class='py-2 px-4'>{item.FillShelves} minuten per coli</td>
+                    <td class='py-2 px-4'>{item.Cashier}</td>
+                    <td class='py-2 px-4'>{item.Fresh}</td>
+                    <td class='py-2 px-4'>{item.Fronting} seconden per meter</td>";
+        }, searchTerm, page);
+
+        ViewBag.HtmlTable = htmlTable;
+
+        return View();
     }
 
     public async Task<IActionResult> Create(Boolean lastWeek)
--- bumbo/obj/Debug/net8.0/bumbo.AssemblyInfo.cs
+++ bumbo/obj/Debug/net8.0/bumbo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bumbo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c8f426e0ed991fe11dea963a5e5dac2e9fc7df1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49753cbf029424b058a4dbf4fab6d5f34b97ea13")]
 [assembly: System.Reflection.AssemblyProductAttribute("bumbo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bumbo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
