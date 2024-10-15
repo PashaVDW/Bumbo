d8e1027194fe38d7f0b5208e721c458fa799fa7d
ant-george <agross@live.nl>
2024-10-15 12:20:24 +0200

    Nog een keer migratiosn weg

--- bumbo/obj/Debug/net8.0/bumbo.AssemblyInfo.cs
+++ bumbo/obj/Debug/net8.0/bumbo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bumbo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18bbcc02e16ed68d7b3d7ca900498dd084038405")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6d1d74d825ca195b7cf44d3e31a3e0f55d26ecd")]
 [assembly: System.Reflection.AssemblyProductAttribute("bumbo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bumbo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
