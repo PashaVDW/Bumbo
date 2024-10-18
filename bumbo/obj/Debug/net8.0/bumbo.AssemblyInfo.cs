0fd4cf315b5dd5d6a54ffd555ab0a4f750c9def2
ant-george <agross@live.nl>
2024-10-18 11:38:23 +0200

    GitIgnore missing

--- bumbo/obj/Debug/net8.0/bumbo.AssemblyInfo.cs
+++ bumbo/obj/Debug/net8.0/bumbo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bumbo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac49485f12479313e5af6410f929a1024c4d8453")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3aed98c209675004a12dcd0cae6f82190df3e09")]
 [assembly: System.Reflection.AssemblyProductAttribute("bumbo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bumbo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
