ea0bff1613294f83f0633123b46c0c8f92583bb6
Pasha Ahmad <pasha.pasha12e@gmail.com>
2024-10-28 11:31:46 +0100

    wip

--- DataLayer/Repositories/PrognosisRepositorySql.cs
+++ DataLayer/Repositories/PrognosisRepositorySql.cs
@@ -1,6 +1,7 @@
 ﻿using bumbo.Data;
 using bumbo.Models;
 using DataLayer.Interfaces;
+using DataLayer.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Globalization;
 
@@ -13,6 +14,47 @@ namespace DataLayer.Repositories
         public PrognosisRepositorySql(BumboDBContext context)
         {
             _context = context;
+        }
+
+        public void AddPrognosis(List<Days> days, List<int> CustomerAmount, List<int> PackagesAmount, int week, int year)
+        {
+            var prognosis = new Prognosis
+            {
+                BranchId = 1,
+                WeekNr = week,
+                Year = year
+            };
+
+            _context.Prognoses.Add(prognosis);
+            _context.SaveChanges();
+
+            if (days.Count == CustomerAmount.Count && days.Count == PackagesAmount.Count)
+            {
+                foreach (var day in days)
+                {
+                    // Check if the day exists in the Days table
+                    if (!_context.Days.Any(d => d.Name == day.Name))
+                    {
+                        throw new InvalidOperationException($"Day '{day.Name}' does not exist in the Days table.");
+                    }
+
+                    var prognosisHasDays = new Prognosis_has_days
+                    {
+                        Days_name = day.Name,
+                        PrognosisId = prognosis.PrognosisId,
+                        CustomerAmount = CustomerAmount[days.IndexOf(day)],
+                        PackagesAmount = PackagesAmount[days.IndexOf(day)]
+                    };
+
+                    _context.Prognosis_Has_Days.Add(prognosisHasDays);
+                }
+
+                _context.SaveChanges();
+            }
+            else
+            {
+                throw new ArgumentException("Days, CustomerAmount, and PackagesAmount lists must have the same number of elements.");
+            }
         }
 
         public List<Prognosis> GetAllPrognosis()
