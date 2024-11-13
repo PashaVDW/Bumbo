94f154fdc28c54d40c52aecc3b623ab557ca4bf6
DouweP <144326129+DouweP@users.noreply.github.com>
2024-11-13 20:52:51 +0100

    navigatie werkt zoals in terugblikken

---
+++ bumbo/ViewModels/PrognosisDay.cs
@@ -0,0 +1,11 @@
+﻿namespace bumbo.ViewModels
+{
+    public class PrognosisDay
+    {
+        public string DayName { get; set; }
+        public DateTime Date { get; set; }
+        public int Hours { get; set; }
+        public int Employees { get; set; }
+
+    }
+}
