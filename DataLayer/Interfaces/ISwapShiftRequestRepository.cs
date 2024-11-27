90fb7369e2f6ad26f3e5363cbe8077446e821c54
ant-george <agross@live.nl>
2024-11-27 11:00:34 +0100

    Eerste opzet controller

---
+++ DataLayer/Interfaces/ISwapShiftRequestRepository.cs
@@ -0,0 +1,16 @@
+﻿using bumbo.Models;
+using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Interfaces
+{
+    public interface ISwapShiftRequestRepository
+    {
+        public List<SwitchRequest> GetAllIncomingRequests(string employeeId);
+        public List<SwitchRequest> GetAllOutgoingRequests(string employeeId);
+    }
+}
---
+++ bumbo/Controllers/SwapShiftController.cs
@@ -0,0 +1,37 @@
+﻿using bumbo.Models;
+using DataLayer.Interfaces;
+using DataLayer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace bumbo.Controllers
+{
+    public class SwapShiftController : Controller
+    {
+        private readonly UserManager<Employee> _userManager;
+
+        public SwapShiftController(UserManager<Employee> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index(string searchTerm, int page = 1)
+        {
+            Employee user = await _userManager.GetUserAsync(User);
+
+            if (user == null)
+            {
+                return RedirectToAction("AccessDenied", "Home");
+            }
+
+            string employeeId = user.Id;
+
+            List<SwitchRequest> incomingRequests = _branchRequestsEmployeeRepository.GetAllIncomingRequests(employeeId);
+            List<SwitchRequest> outgoingRequests = _branchRequestsEmployeeRepository.GetAllOutgoingRequests(employeeId);
+
+
+            return View();
+        }
+    }
+}
