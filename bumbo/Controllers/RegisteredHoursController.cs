fe8c34d61cc3d20042fafa6ee72b2e5bb0af7f88
NLYanick <yanick.plaggenmarsch@gmail.com>
2024-12-22 15:34:28 +0100

    Uren, maar zonder CAO regels

--- bumbo/Controllers/RegisteredHoursController.cs
+++ bumbo/Controllers/RegisteredHoursController.cs
@@ -6,6 +6,7 @@ using DataLayer.Interfaces;
 using DataLayer.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Metrics;
 using System.Drawing.Printing;
 using System.Globalization;
 
@@ -60,16 +61,18 @@ namespace bumbo.Controllers
         {
             Document document = new Document();
 
-            Page page = new Page(PageSize.Letter, PageOrientation.Portrait, 54.0f);
+            Page page = new Page(PageSize.A4, PageOrientation.Portrait, 54.0f);
             document.Pages.Add(page);
 
-            AddText(page, registeredHours, employees);
+            AddText(page, registeredHours, employees, document);
 
             document.Draw(@"Views/PDF/EmployeesHoursOverview.pdf");
         }
 
-        private void AddText(Page page, List<RegisteredHours> registeredHours, List<Employee> employees)
+        private void AddText(Page page, List<RegisteredHours> registeredHours, List<Employee> employees, Document document)
         {
+            int pageHeight = 700;
+
             int width = 500;
             int height = 100;
             int fontSize = 18;
@@ -77,31 +80,68 @@ namespace bumbo.Controllers
             int multiplier = 40;
 
             int y = 0;
-            foreach (RegisteredHours hour in registeredHours)
+            int counter = 0;
+            foreach (Employee employee in employees) 
             {
-                Employee emp = GetEmployeeForHour(hour, employees);
-                if (emp == null) 
-                { 
-                    return; 
-                }
-                string text = $"{emp.FirstName} {emp.MiddleName} {emp.LastName}: {hour.StartTime} - {hour.EndTime}";
-                Label label = new Label(text, x, y * multiplier, width, height, Font.Helvetica, fontSize, TextAlign.Center);
-                page.Elements.Add(label);
+                string text = $"{employee.FirstName} {employee.LastName} ({employee.Email}): ";
+                Label employeeName = new Label(text, x, y * multiplier, width, height, Font.Helvetica, fontSize, TextAlign.Left);
+                page.Elements.Add(employeeName);
+                counter = 0;
+                foreach (RegisteredHours hour in registeredHours)
+                {
+                    text = SetLabelText(text, hour, counter, employee);
 
+                    counter++;
+                }
+                if (!text.Equals($"{employee.FirstName} {employee.LastName} ({employee.Email}): "))
+                {
+                    y++;
+                    if (y * multiplier >= pageHeight)
+                    {
+                        page = NextPage(page, document);
+                        y = 0;
+                    }
+
+                    Label hoursText = new Label(text, x, y * multiplier, width, height, Font.Helvetica, fontSize, TextAlign.Left);
+                    page.Elements.Add(hoursText);
+                    y++;
+                }
                 y++;
+                if (y * multiplier >= pageHeight)
+                {
+                    page = NextPage(page, document);
+                    y = 0;
+                }
             }
         }
-
-        private Employee GetEmployeeForHour(RegisteredHours hour, List<Employee> employees)
+        private string SetLabelText(string text, RegisteredHours hour, int counter, Employee employee)
         {
-            foreach (Employee employee in employees)
+            if (employee.Id == hour.EmployeeId &&
+                        !(hour.EndTime.Minute - hour.StartTime.Minute == 0 && hour.EndTime.Hour - hour.StartTime.Hour == 0))
             {
-                if (employee.Id == hour.EmployeeId)
+                if (counter == 0)
                 {
-                    return employee;
+                    text = $"";
+                }
+                if (hour.EndTime.Hour - hour.StartTime.Hour == 0)
+                {
+                    text += $" {hour.EndTime.Minute - hour.StartTime.Minute} minuten";
+                }
+                else
+                {
+                    text += $" {hour.EndTime.Hour - hour.StartTime.Hour} uur";
                 }
             }
-            return null;
+            return text;
+        }
+
+        private Page NextPage(Page page, Document document)
+        {
+            Page nextPage = new Page(PageSize.A4, PageOrientation.Portrait, 54.0f);
+            page = nextPage;
+            document.Pages.Add(nextPage);
+
+            return page;
         }
     }
 }
